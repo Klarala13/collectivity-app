a211a7cb394642ee2b72a98c774397cb9d6dab27
Klara <clara.sanchez@promocionesfarma.com>
2023-10-20 22:24:52 +0200

    fixed app

---
+++ collectivity/src/screens/AddItemForm.tsx
@@ -0,0 +1,43 @@
+import Container from "../components/Container";
+import React, { useCallback, useEffect, useState } from "react";
+
+import { useTheme } from "../hooks";
+import { Input } from "../components";
+
+const AddItemForm = () => {
+  const { assets, colors, sizes } = useTheme();
+  //Should display a form for adding an item
+  //Should make call to backend to add that item to DB
+  //Should have fields for item name, user´s name, item description, item picture and a you can choose wheather you want to give away the item or share it.
+  //If you want to share the item, you should be able to choose if it´s for one day, one week or one month.
+  return (
+    <Container>
+      <Input
+        autoCapitalize="none"
+        marginBottom={sizes.m}
+        label="Item name"
+        placeholder="Item name"
+      />
+      <Input
+        autoCapitalize="none"
+        marginBottom={sizes.m}
+        label="Description"
+        placeholder="Description"
+      />
+      <Input
+        autoCapitalize="none"
+        marginBottom={sizes.m}
+        label="Location"
+        placeholder="Location"
+      />
+      <Input
+        autoCapitalize="none"
+        marginBottom={sizes.m}
+        label="Category"
+        placeholder="Category"
+      />
+    </Container>
+  );
+};
+
+export default AddItemForm;
