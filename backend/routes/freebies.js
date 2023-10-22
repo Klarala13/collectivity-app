08397d349150aac5acd4e6a1ba74406ee034535c
fridavbg <fperssontech@gmail.com>
2023-10-22 17:42:21 +0200

    Freebie CRUD

--- backend/routes/freebies.js
+++ backend/routes/freebies.js
@@ -81,6 +81,18 @@ router.get("/user/:user_id", async (req, res, next) => {
     }
 });
 
+/**
+ * Freebie ROUTE
+ *  /create:
+ *   get:
+ *     summary: Create a new Freebie
+ *     description: Create a new freebie by user id
+ *  @param req  The incoming request.
+ *  @param res  The outgoing response.
+ *  @param next Next to call in chain of middleware.
+ *
+ * @returns {void}
+ */
 router.post("/create", async (req, res, next) => {
     const { user_id, item, description, image, zip_code, location, category } =
         req.body;
@@ -97,6 +109,78 @@ router.post("/create", async (req, res, next) => {
         });
 
         res.json({ message: "Freebie added", data: newFreebie });
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+/**
+ * Freebie ROUTE
+ *  /update:
+ *   get:
+ *     summary: Update Freebie by item id
+ *     description: Update any field in a freebie row
+ *  @param req  The incoming request.
+ *  @param res  The outgoing response.
+ *  @param next Next to call in chain of middleware.
+ *
+ * @returns {void}
+ */
+router.put("/update", async (req, res, next) => {
+    const {
+        user_id,
+        item_id,
+        item,
+        description,
+        image,
+        zip_code,
+        location,
+        category,
+    } = req.body;
+
+    try {
+        const updateFreebie = await Freebie.update(
+            {
+                user_id: user_id,
+                item: item,
+                description: description,
+                image: image,
+                zip_code: zip_code,
+                location: location,
+                category: category,
+            },
+            { where: { item_id: item_id } }
+        );
+
+        res.json({ message: "Freebie updated", data: updateFreebie });
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+/**
+ * Freebie ROUTE
+ *  /delete:
+ *   get:
+ *     summary: DELETE Freebie by item id
+ *     description: Delete a freebie row
+ *  @param req  The incoming request.
+ *  @param res  The outgoing response.
+ *  @param next Next to call in chain of middleware.
+ *
+ * @returns {void}
+ */
+router.delete("/delete", async (req, res, next) => {
+    const {item_id} = req.body;
+
+    try {
+        const freebieToDelete = await Freebie.destroy(
+            { where: { item_id: item_id } }
+        );
+
+        res.json({ message: "Freebie deleted", data: freebieToDelete });
     } catch (error) {
         next(error);
     }
